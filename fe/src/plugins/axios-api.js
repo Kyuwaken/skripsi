d04e412947fa2f132bafc560ddccf02715c13cf1
Vincentius <40225060+Kyuwaken@users.noreply.github.com>
2023-04-26 14:40:27 +0700

    [FE] change axios

--- fe/src/plugins/axios-api.js
+++ fe/src/plugins/axios-api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const getAPI = axios.create({
-  baseURL: "https://backend-skripsi-production.up.railway.app/",
+  baseURL: "https://skripsi-production.up.railway.app/",
   timeout: 170000,
   withCredentials: true
 });
